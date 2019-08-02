c1472515602afd376a4826ce9654e14d940f8fe6
Rob Simmonds <robert.simmonds@uct.ac.za>
2019-08-02 06:46:06 +0000

    Remove attempt to open config from file system.

--- scripts/carta_run_local.sh
+++ scripts/carta_run_local.sh
@@ -8,6 +8,6 @@ myname = $(whoami)
 
 mkdir logs
 
-echo "Home is set to $HOME : $LOGNAME : $USER ; $myname"  > logs/carta_start_log.$$ 2>&1
+echo "Home is set to $HOME : socket is $CARTA_USER_PORT"  > logs/carta_start_log.$$ 2>&1
 
-/usr/local/CARTA/bin//carta_backend -threads 4 -root /carta_share -base /carta_share/users/$1 -exit_after 0 -init_exit_after 10 -port $CARTA_USER_PORT -read_json_file ${HOME}/.carta/config > logs/carta_log.$$ 2>&1
+/usr/local/CARTA/bin//carta_backend -threads 4 -root /carta_share -base /carta_share/users/$1 -exit_after 0 -init_exit_after 10 -port $CARTA_USER_PORT -use_mongodb 1 > logs/carta_log.$$ 2>&1
