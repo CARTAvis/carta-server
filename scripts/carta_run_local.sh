f4f1c14fadf250044dee41ec4dd5d93ea82cfe3a
Rob Simmonds <robert.simmonds@uct.ac.za>
2019-08-02 09:47:04 +0000

    Clean up.

--- scripts/carta_run_local.sh
+++ scripts/carta_run_local.sh
@@ -10,4 +10,4 @@ mkdir logs
 
 echo "Home is set to $HOME : socket is $CARTA_USER_PORT"  > logs/carta_start_log.$$ 2>&1
 
-/usr/local/CARTA/bin//carta_backend -threads 4 -root /carta_share -base /carta_share/users/$1 -exit_after 0 -init_exit_after 10 -port $CARTA_USER_PORT -use_mongodb 1 > logs/carta_log.$$ 2>&1
+/usr/local/CARTA/bin/carta_backend -threads 4 -root /carta_share -base /carta_share/users/$1 -exit_after 0 -init_exit_after 10 -port $CARTA_USER_PORT -use_mongodb 1 > logs/carta_log.$$ 2>&1
