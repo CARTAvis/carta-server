85db21874c9b5b761fa5e19478014db61c935fdb
Rob Simmonds <robert.simmonds@uct.ac.za>
2019-09-18 11:51:53 +0000

    Added support for external users that only appear in the DB; these don't need system accounts.

--- scripts/auth/index.php
+++ scripts/auth/index.php
@@ -22,31 +22,43 @@
     $myobj->token = $document['token'];
     $socket_string =  "socket" . $document['socket'];
     $myobj->socket = $socket_string;
+    $myobj->usertype = $document['usertype'];
   }
 //  echo json_encode($myobj);
 
   $username = $myobj->username;
-
   $socket = $myobj->socket;
-  $auth_string = $username.":".$password;
-  $suexecstring = "https://carta-dev.idia.ac.za/~${username}/cgi-bin/carta.pl?socket=$socket";      
-   
-  $ch = curl_init();
 
-  // set URL and other appropriate options
-  curl_setopt($ch, CURLOPT_URL, $suexecstring);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-  curl_setopt($ch, CURLOPT_USERPWD, $auth_string);
-  curl_setopt($ch, CURLOPT_NOBODY, 0);
+  if( !strcmp( $myobj->usertype, "external" ) ) {
+     $dbphash = $document['password'];
+     if( password_verify($password, $dbphash) ) { // Correct password.
+        $myobj2->username = $username;
+        $myobj2->socket = $myobj->socket;
+	$myobj2->token = $myobj->token;
+     	echo json_encode($myobj2);
+     } else { // Wrong password.
+        http_response_code(401);
+     }
+  } else { // System user so start a session for them.
+    $auth_string = $username.":".$password;
+    $suexecstring = "https://carta-dev.idia.ac.za/~${username}/cgi-bin/carta.pl?socket=$socket";    
+    $ch = curl_init();
 
-  // grab URL and pass it to the browser
-  curl_exec($ch);
-  
-  // forward the response code
-  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  http_response_code($http_code);
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $suexecstring);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+    curl_setopt($ch, CURLOPT_USERPWD, $auth_string);
+    curl_setopt($ch, CURLOPT_NOBODY, 0);
 
-  // close cURL resource, and free up system resources
-  curl_close($ch);
+    // grab URL and pass it to the browser
+    curl_exec($ch);
+
+    // forward the response code
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    http_response_code($http_code);
+ 
+    // close cURL resource, and free up system resources
+    curl_close($ch);
+  }
 ?>
